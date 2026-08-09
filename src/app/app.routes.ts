3aecfeb5e27816bd36f6881595c78e623603b469
hecjav1992 <easydata10@gmail.com>
2026-08-09 00:34:46 -0500

    modulo de bi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,60 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BiDashboardComponent } from './pages/inteligencia-negocio/bi-dashboard.component';
 import { IngresosComponent } from './pages/ingresos/ingresos.component';
 import { DiagnosticosComponent } from './pages/diagnosticos/diagnosticos.component';
+
 import { ShellComponent } from './layout/shell.component';
 import { authGuard } from './services/auth.guard';
+
 export const routes: Routes = [
- {path:'login',component:LoginComponent},
- {path:'',component:ShellComponent,canActivate:[authGuard],children:[
-   {path:'',component:DashboardComponent},
-   {path:'ingresos',component:IngresosComponent},
-   {path:'diagnosticos',component:DiagnosticosComponent}
- ]},
- {path:'**',redirectTo:''}
+
+  // LOGIN
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+
+  // SISTEMA PROTEGIDO
+  {
+    path: '',
+    component: ShellComponent,
+    canActivate: [authGuard],
+
+    children: [
+
+      // DASHBOARD
+      {
+        path: '',
+        component: DashboardComponent
+      },
+
+      // INGRESO DE EQUIPOS
+      {
+        path: 'ingresos',
+        component: IngresosComponent
+      },
+
+      // DIAGNÓSTICOS
+      {
+        path: 'diagnosticos',
+        component: DiagnosticosComponent
+      },
+
+      // INTELIGENCIA DE NEGOCIO
+      {
+        path: 'inteligencia-negocio',
+        component: BiDashboardComponent
+      }
+
+    ]
+  },
+
+  // CUALQUIER RUTA DESCONOCIDA
+  {
+    path: '**',
+    redirectTo: ''
+  }
+
 ];
